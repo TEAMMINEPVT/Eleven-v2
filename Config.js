ecc073b35de62d4b44db76c2c23c9370ceff8004
TEAM MINE <teamminepvt@gmail.com>
2025-02-02 11:46:50 +0530

    Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,10 @@
 const { ButtonStyle } = require("discord.js");
 module.exports = {
-    Token: "",
+    Token: "MTMwMjU2MzYzOTg4MjE1ODA5MQ.G-z4Yq.oUtNGIQnXtqc2yT438zjD-un6uR43jWxhbBrF8",
     Prefix: ".",
     Client: {
-        ID: "",
-        Secret: "",
+        ID: "1302563639882158091",
+        Secret: "uRgSkJThgAUT4In_5zoB31gtWMebwdv0",
     },
     button: {
         grey: ButtonStyle.Secondary,
@@ -14,20 +14,20 @@ module.exports = {
         green: ButtonStyle.Success,
     },
     spotify: {
-        ID: "",
-        Secret: "",
+        ID: "bf5ee2a72bae40ffadc71a47280e5ff9",
+        Secret: "053469ffeb3844079fab734ebf3090c2",
     },
     Api: {
-        Topgg: "",
+        Topgg: "https://top.gg",
     },
-    MongoData: "",
-    EmbedColor: "",
-    Owners: [""],
+    MongoData: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
+    EmbedColor: "#000099",
+    Owners: ["1193934430872547363"],
     Nodes: [
         {
-            name: "",
-            url: "",
-            auth: "",
+            name: "TM1",
+            url: "lavahatry4.techbyte.host",
+            auth: "NAIGLAVA-dash.techbyte.host",
             secure: false,
         },
         {
@@ -51,7 +51,7 @@ module.exports = {
     links: {
         invite: "",
         bg: "",
-        support: "",
+        support: "https://dsc.gg/teamminepvt",
         vote: "",
         banner: "",
         spotify: "",
